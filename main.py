736c195f85ee4c088a14cfa7d91f5418d8906bc0
gpanagioto <ge.panagioto@gmail.com>
2022-11-29 11:43:17 +0100

    Latest updates

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import reverse_geocode as revgc
 import plotly.express as px
- 
+import pandas as pd
+import holidays
+
 def FindingCity(row,point):
     if point.lower() == 'start':
         coordinates = [(row['start station latitude'],row['start station longitude'])]
@@ -9,6 +11,59 @@ def FindingCity(row,point):
     
     return revgc.search(coordinates)[0]['city']
 
+def DatetimeInterval(df,freq):
+    df1 = df.set_index('starttime')
+    df1 = pd.get_dummies(df1, columns=['usertype'])
+    df1 = df1.resample(rule=freq, label='left', origin='start_day').sum()
+    pickups = df1.loc[:,['usertype_Customer','usertype_Subscriber']]
+    pickups['pickups'] = pickups.loc[:,['usertype_Customer','usertype_Subscriber']].sum(axis=1)
+    #pickups = pickups['tripduration'].groupby(pd.Grouper(freq=freq, label='left', origin='start_day')).count()    
+    pickups = pd.DataFrame(pickups)
+    #pickups.rename(columns={'tripduration':'pickups'}, inplace=True)
+    
+    return pickups
+
+def PreciptypeMap(row):
+    if row == 'rain':
+        return 2
+    elif row == 'rain,snow':
+        return 3
+    elif row == 'snow':
+        return 4
+    elif row in ['freezingrain','snow,ice']:
+        return 5
+    else:
+        return 0
+
+def ConditionsMap(row):
+    if row in ['Overcast','Partially cloudy','Clear']:
+        return 1
+    elif row in ['Rain, Overcast','Rain, Partially cloudy','Snow, Partially cloudy']:
+        return 2
+    elif row in ['Snow, Rain, Overcast','Snow, Overcast','Snow, Ice, Overcast']:
+        return 3
+
+def MergingDataFrames(wd, df):
+    pickups = pd.merge(wd, df, left_index=True, right_index=True)
+    return pickups
+
+def DataPreprocess(pickup,season_dict):
+    pickup['workingday'] = pickup.index.map(lambda row: 1 if row.dayofweek < 5 else 0)
+    pickup['holiday'] = pickup.index.map(lambda row: 1 if row in holidays.US() else 0)
+    pickup['month'] = pickup.index.month
+    pickup['hour'] = pickup.index.hour
+    pickup['minute'] = pickup.index.minute
+    pickup.reset_index(drop=True, inplace=True)
+    pickup['season'] = pickup['month'].map(season_dict)
+    
+    return pickup
+
+def Lagging(df, minutes):
+    for i,v in enumerate(range(minutes,120+1,minutes)):
+        df['lag(pickups,{})'.format(i+1)] = df['pickups'].shift(i+1)
+        
+    return df
+    
 def get_location_interactive(df, mapbox_style="open-street-map"):
     """Return a map with markers for houses based on lat and long.
     
